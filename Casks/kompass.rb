2e85a6eac95b45285fcfbcf226a5cc01da86e981
erango <goldin@bizzabo.com>
2026-06-04 12:44:00 +0300

    kompass 1.0.4

--- Casks/kompass.rb
+++ Casks/kompass.rb
@@ -1,6 +1,6 @@
 cask "kompass" do
-  version "1.0.3"
-  sha256 "6167467b21db8e7e9d3a1d54b1bce088c4c2af38320ac72163ecbce3b66d9f9e"
+  version "1.0.4"
+  sha256 "f4b55a1f86988890f8f6306c5f53e43e5436299d6c846dea7499f1f5b088fe24"
 
   url "https://github.com/erango/kompass/releases/download/v#{version}/Kompass-#{version}.dmg"
   name "Kompass"
